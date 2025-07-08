e642bcc83cd11e42ef8447c0b08c65c47be11542
Warwick Allen <warwick@datumprocess.co.nz>
2025-07-08 23:57:42 +1200

    Use pipeline inputs in tests
    - In the Pester tests, pass the strings into the functions using a pipeline, rather than using the `-String` parameter.
    - Added a line prefix and suffix test, and one other test.

--- Split-StringAtWordBoundaries.Tests.ps1
+++ Split-StringAtWordBoundaries.Tests.ps1
@@ -8,33 +8,51 @@ Describe 'Split-StringAtWordBoundaries' {
         Should -Match '\b0\.\d+\.\d+$'
     }
     It 'Wraps string using dot delimiter and length 12' {
-        Split-StringAtWordBoundaries -String 'aaa.bbb...ccc.ddd.eee.fff' -Length 12 -WordDelimiter '(\.)`.*' |
+        'aaa.bbb...ccc.ddd.eee.fff' |
+        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)`.*' |
         Should -Be "aaa.bbb.`nccc.ddd.eee.`nfff"
     }
 
     It 'Wraps sentence with whitespace default delimiter and length 10' {
-        Split-StringAtWordBoundaries -String 'This is a long sentence to wrap.' -Length 10 |
+        'This is a long sentence to wrap.' |
+        Split-StringAtWordBoundaries -Length 10 |
         Should -Be "This is a`nlong`nsentence`nto wrap."
     }
 
     It 'Wraps using dot delimiter and custom separator with length 12' {
         $sep = ([char]10 + '123')
-        Split-StringAtWordBoundaries -String 'aaa.bbb...cccc.ddd.eee.fff.hhh.iii' -Length 12 -WordDelimiter '(\.)\.*' -LineSeparator $sep |
+        'aaa.bbb...cccc.ddd.eee.fff.hhh.iii' |
+        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)\.*' -LineSeparator $sep |
         Should -Be "aaa.bbb.`n123cccc.ddd.`n123eee.fff.`nhhh.123iii"
     }
 
     It 'Wraps long sentence using custom delimiter "(.) " and length 98' {
-        Split-StringAtWordBoundaries -Length 98 -String 'The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed the specified length.' -WordDelimiter '(.) ' |
-        Should -Be "The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed`nthe specified length."
+        'The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed the specified length.' |
+        Split-StringAtWordBoundaries -Length 98 -WordDelimiter '(.) ' |
+        Should -Be "The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line`ndoes not exceed the specified length."
+    }
+
+    It 'Wraps long sentence using custom delimiter "(.)" and length 96' {
+        'The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed the specified length.' |
+        Split-StringAtWordBoundaries -Length 96 -WordDelimiter '(.)' |
+        Should -Be "The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each lin`ne does not exceed the specified length."
     }
 
     It 'Wraps name with hyphen and length 12' {
-        Split-StringAtWordBoundaries -String 'Mr Wright-Rong has a double-barrelled name.' -Length 12 |
+        'Mr Wright-Rong has a double-barrelled name.' |
+        Split-StringAtWordBoundaries -Length 12 |
         Should -Be "Mr Wright-`nRong has a`ndouble-`nbarrelled`nname."
     }
 
     It 'Wraps dot-delimited string with length 8' {
-        Split-StringAtWordBoundaries -String '. a.bb cc.ddd eee.ffff gggg.hhh iii.jj kk.l . ' -Length 8 -WordDelimiter '`.' |
+        '. a.bb cc.ddd eee.ffff gggg.hhh iii.jj kk.l . ' |
+        Split-StringAtWordBoundaries -Length 8 -WordDelimiter '`.' |
         Should -Be ". a`nbb cc`nddd eee`nffff gggg`nhhh iii`njj kk`nl . "
+    }
+
+    It 'Wraps string using a line prefix and a line suffix' {
+        'The quick brown fox jumps over the lazy dog.' |
+        Split-StringAtWordBoundaries -Length 16 -LinePrefix '  ~' -LineSuffix '~' |
+        Should -Be "  ~The quick~`n  ~brown fox~`n  ~jumps over~`n  ~the lazy dog."
     }
 }
