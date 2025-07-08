2cab0b1b531e7c3718de0b016bb2d7fb9bd07978
Warwick Allen <warwick@datumprocess.co.nz>
2025-07-09 00:35:13 +1200

    Add -LineSuffix to existing tests

--- Split-StringAtWordBoundaries.Tests.ps1
+++ Split-StringAtWordBoundaries.Tests.ps1
@@ -7,11 +7,6 @@ Describe 'Split-StringAtWordBoundaries' {
         (Split-StringAtWordBoundaries -Version)[0] |
         Should -Match '\b0\.\d+\.\d+$'
     }
-    It 'Wraps string using dot delimiter and length 12' {
-        'aaa.bbb...ccc.ddd.eee.fff' |
-        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)`.*' |
-        Should -Be "aaa.bbb.`nccc.ddd.eee.`nfff"
-    }
 
     It 'Wraps sentence with whitespace default delimiter and length 10' {
         'This is a long sentence to wrap.' |
@@ -19,22 +14,28 @@ Describe 'Split-StringAtWordBoundaries' {
         Should -Be "This is a`nlong`nsentence`nto wrap."
     }
 
+    It 'Wraps string using dot delimiter and length 12' {
+        'aaa.bbb...ccc.ddd.eee.fff' |
+        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)`.*' -LineSuffix '\1' |
+        Should -Be "aaa.bbb.`nccc.ddd.eee.`nfff"
+    }
+
     It 'Wraps using dot delimiter and custom separator with length 12' {
         $sep = ([char]10 + '123')
         'aaa.bbb...cccc.ddd.eee.fff.hhh.iii' |
-        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)\.*' -LineSeparator $sep |
+        Split-StringAtWordBoundaries -Length 12 -WordDelimiter '(\.)\.*' -LineSuffix '\1' -LineSeparator $sep |
         Should -Be "aaa.bbb.`n123cccc.ddd.`n123eee.fff.`nhhh.123iii"
     }
 
     It 'Wraps long sentence using custom delimiter "(.) " and length 98' {
         'The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed the specified length.' |
-        Split-StringAtWordBoundaries -Length 98 -WordDelimiter '(.) ' |
+        Split-StringAtWordBoundaries -Length 98 -WordDelimiter '(.) ' -LineSuffix '\1' |
         Should -Be "The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line`ndoes not exceed the specified length."
     }
 
     It 'Wraps long sentence using custom delimiter "(.)" and length 96' {
         'The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each line does not exceed the specified length.' |
-        Split-StringAtWordBoundaries -Length 96 -WordDelimiter '(.)' |
+        Split-StringAtWordBoundaries -Length 96 -WordDelimiter '(.)' -LineSuffix '\1' |
         Should -Be "The Split-StringAtWordBoundaries function breaks a string into multiple lines, ensuring each lin`ne does not exceed the specified length."
     }
 
